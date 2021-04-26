27caed53e3fda2b6347da558d37ad3747387931d
Vanilla <neko@hydev.org>
2021-04-26 21:07:46 +0800

    [+] Check args validtion in main function.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,18 @@
-fun main() {
-    println(greeting("LooksFun"))
-}
+import parser.argsValid
 
-fun greeting(name: String) =
-    "Hello, $name"
+private const val USAGE =
+    """lkf <TypeA> (1+) - <TypeC>, 
+Example: lkf int - long, lkf long double - String. """
+
+// https://nodejs.org/api/process.html#process_exit_codes
+const val INVALID_ARGUMENT = 9
+
+fun main() {
+    val argv = process.argv
+    val args = argv.slice(2 until argv.size)
+
+    if (!argsValid(args)) {
+        console.log("Usage: ${USAGE.trimIndent()}")
+        process.exit(INVALID_ARGUMENT)
+    }
+}
