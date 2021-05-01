1156c2e30200acd7966ea3142400487594323921
Vanilla <neko@hydev.org>
2021-05-01 18:10:24 +0800

    [F] Fix a judgment bug in UnaryOperatorParser#check.

--- src/main/kotlin/parser/UnaryOperatorParser.kt
+++ src/main/kotlin/parser/UnaryOperatorParser.kt
@@ -6,9 +6,7 @@ object UnaryOperatorParser : FIParser() {
     override fun commonName(): String = "UnaryOperator"
 
     override fun check(argsList: List<String>, returnType: String): Boolean {
-        if (argsList.size == 1) return true
-        if (argsList.first() == returnType) return true
-        return false
+        return argsList.size == 1 && argsList[0] == returnType
     }
 
     override fun internalParse(argsList: List<String>, returnType: String): ParseResult {
