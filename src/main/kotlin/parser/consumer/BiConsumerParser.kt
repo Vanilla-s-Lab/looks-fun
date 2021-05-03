59bf23b459b0fd8f466f28407ca4a4dff5ac998b
Vanilla <neko@hydev.org>
2021-05-03 13:36:45 +0800

    [+] Finish consumer parser(C & BiC).

---
+++ src/main/kotlin/parser/consumer/BiConsumerParser.kt
@@ -0,0 +1,22 @@
+package parser.consumer
+
+import ParseResult
+import parser.FIParser
+import kotlin.math.abs
+
+object BiConsumerParser : FIParser() {
+    override fun check(argsList: List<String>, returnType: String): Boolean {
+        return (argsList.size == 2 &&
+                returnType == VOID)
+    }
+
+    override fun internalParse(argsList: List<String>, returnType: String): ParseResult {
+        val probeStdTypeIndex = argsList.indexOfFirst { it in stdTypeList }
+        if (probeStdTypeIndex != -1) { // Found at least one type is std.
+            val anotherIndex = abs((argsList.size - 1) - probeStdTypeIndex)
+            return "Obj${argsList.get(probeStdTypeIndex)}${commonName()}" to
+                    listOf(argsList[anotherIndex]).map { it.filterStdType() }
+        }
+        return commonName() to argsList.map { it.filterStdType() }
+    }
+}
---
+++ src/main/kotlin/parser/consumer/ConsumerParser.kt
@@ -0,0 +1,17 @@
+package parser.consumer
+
+import ParseResult
+import parser.FIParser
+
+object ConsumerParser : FIParser() {
+    override fun check(argsList: List<String>, returnType: String): Boolean {
+        return (argsList.size == 1 &&
+                returnType == VOID)
+    }
+
+    override fun internalParse(argsList: List<String>, returnType: String): ParseResult {
+        val arg = argsList[0]
+        if (arg in stdTypeList) return "${arg.capitalize()}${commonName()}" to emptyList()
+        return commonName() to argsList.map { it.filterStdType() }
+    }
+}
