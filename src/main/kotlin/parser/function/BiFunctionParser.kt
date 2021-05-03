3479c7fbd1f0f3c661479ecf104fb377299080ee
Vanilla <neko@hydev.org>
2021-05-03 11:28:34 +0800

    [+] Finish function parser (BiF & F).

---
+++ src/main/kotlin/parser/function/BiFunctionParser.kt
@@ -0,0 +1,20 @@
+package parser.function
+
+import ParseResult
+import parser.FIParser
+
+object BiFunctionParser : FIParser() {
+    override fun check(argsList: List<String>, returnType: String): Boolean {
+        val allArgs = argsList.toMutableList().apply { add(returnType) }
+        return (argsList.size == 2 && // Has 2 args, and all args not equal at the same time.
+                allArgs.toSet().size != 1) // Because If that's the case, use BinaryOperator instead.
+    }
+
+    override fun internalParse(argsList: List<String>, returnType: String): ParseResult {
+        if (returnType in stdTypeList) // To TypeA BiFunction <T, U>.
+            return "To${returnType.capitalize()}${commonName()}" to argsList.map { it.filterStdType() }
+
+        val allArgs = argsList.toMutableList().apply { add(returnType) }
+        return commonName() to allArgs.map { it.filterStdType() } // CommonName with <T, U, R>.
+    }
+}
---
+++ src/main/kotlin/parser/function/FunctionParser.kt
@@ -0,0 +1,32 @@
+package parser.function
+
+import ParseResult
+import parser.FIParser
+
+object FunctionParser : FIParser() {
+    override fun check(argsList: List<String>, returnType: String): Boolean {
+        return (argsList.size == 1 &&
+                argsList[0] != returnType)
+    }
+
+    override fun internalParse(argsList: List<String>, returnType: String): ParseResult {
+        val arg = argsList[0]
+
+        val argFlag = arg in fiStdTypeList
+        val rTFlag = returnType in fiStdTypeList
+
+        if (argFlag && rTFlag) { // <T, R> are std type, consider (TypeA)To(TypeB)Function.
+            val (first, second) = listOf(arg, returnType).map { it.capitalize() }
+            return "${first}To$second${commonName()}" to emptyList()
+        }
+
+        if (argFlag) // Only arg is stdType, use "TypeAFunction" instead.
+            return "${arg.capitalize()}${commonName()}" to listOf(returnType).map { it.filterStdType() }
+
+        if (rTFlag) // Only return type is stdType, use "ToTypeAFunction" instead.
+            return "To${returnType.capitalize()}${commonName()}" to listOf(arg).map { it.filterStdType() }
+
+        // Common situation, Function<T, R> is [(T) -> R].
+        return commonName() to listOf(argsList[0], returnType).map { it.filterStdType() }
+    }
+}
